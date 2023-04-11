7e842b563724c0614f2f65e9a0f4f4b6ca9bf781
fredbanda <fredcbanda@hotmail.com>
2023-04-11 17:47:29 +0200

    this is a trial repository not mine

---
+++ client/src/store/index.js
@@ -0,0 +1,12 @@
+import { proxy } from 'valtio';
+
+const state = proxy({
+  intro: true,
+  color: '#EFBD48',
+  isLogoTexture: true,
+  isFullTexture: false,
+  logoDecal: './logohead.png',
+  fullDecal: './logohead.png',
+});
+
+export default state;
